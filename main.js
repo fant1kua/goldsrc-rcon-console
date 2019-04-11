f95cf3745f9790fbaab0c29bf28985e7d8f35b24
fant1kua <fant1kua@users.noreply.github.com>
2019-04-11 17:11:29 +0300

    Initial commit

--- main.js
+++ main.js
@@ -16,38 +16,56 @@ const RCON = require('./rcon');
 //        callback();
 //    });
 
-const rcon = new RCON({
-    serverHost: '127.0.0.1',
-    serverPort: 27016,
-    clientHost: '127.0.0.1',
-    clientPort: 41234,
-    password: '12345',
-    timeout: 5000
-});
+//const rcon = new RCON({
+//    serverHost: '127.0.0.1',
+//    serverPort: 27016,
+//    clientHost: '127.0.0.1',
+//    clientPort: 41234,
+//    password: '12345',
+//    timeout: 5000
+//});
+//
+//vorpal
+//    .catch('<command>')
+//    .action(function (args, cb) {
+//        console.log(args.command)
+//        //rcon.execute(args.command)
+//        //    .then(cb)
+//        //    .catch(cb);
+//    });
+//
+//rcon
+//    .connect()
+//    .then(() => rcon.execute(`logaddress_del 127.0.0.1 41234`))
+//    .then(() => rcon.execute(`logaddress_add 127.0.0.1 41234`))
+//    .then(() => {
+//        rcon.on('message', (msg) => {
+//            vorpal.log(msg);
+//        })
+//    })
+//    .catch(console.error);
+//
+//vorpal
+//    .delimiter('rcon$')
+//    .show();
 
 vorpal
-    .catch('<command>')
-    .action(function (args, cb) {
-        console.log(args.command)
-        //rcon.execute(args.command)
-        //    .then(cb)
-        //    .catch(cb);
+    .mode('sql')
+    .delimiter('rcon:')
+    .init(function(args, callback){
+        this.log('Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`.');
+        callback();
+    })
+    .action(function(command, callback) {
+        callback();
     });
 
-rcon
-    .connect()
-    .then(() => rcon.execute(`logaddress_del 127.0.0.1 41234`))
-    .then(() => rcon.execute(`logaddress_add 127.0.0.1 41234`))
-    .then(() => {
-        rcon.on('message', (msg) => {
-            vorpal.log(msg);
-        })
-    })
-    .catch(console.error);
+vorpal
+    .show();
 
 vorpal
-    .delimiter('rcon$')
-    .show();
+    .delimiter('')
+    .exec('sql');
 
 
 
