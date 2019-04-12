cac45380de5eed33b000b8f50ed060d272611e6d
fant1kua <fant1kua@users.noreply.github.com>
2019-04-12 13:12:50 +0300

    Refactoring

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const vorpal = require('vorpal')();
 const chalk = require('chalk');
 const program = require('commander');
+const spinner = require('ora')();
 const checkLocalhost = require('check-localhost');
 
 const RCON = require('./rcon');
@@ -10,7 +11,72 @@ const REGEX_LOG = /^log\sL\s(\d{2}\/\d{2}\/\d{4}\s-\s\d{2}:\d{2}:\d{2}):\s(.+)/;
 const REGEX_RCON = /^Rcon:\s/;
 
 const init = (host, port, password) => {
+	spinner.color = 'yellow';
+	spinner.text = 'Connecting to server';
+	spinner.start();
+
 	const rcon = new RCON();
+
+	const initConsole = () => {
+		vorpal
+			.catch('[command...]')
+			.action(function (args, cb) {
+				if (rcon.connected) {
+					rcon.execute(args.command.join(' '))
+						.then(cb)
+						.catch(cb);
+				}
+			});
+
+		vorpal.command('meta [command]')
+			.autocomplete(['version', 'list'])
+			.action(function (args, cb) {
+				if (rcon.connected) {
+					rcon.execute(`meta ${args.command}`)
+						.then(cb)
+						.catch(cb);
+				}
+			});
+
+		vorpal.command('amxx [command]')
+			.autocomplete(['version', 'list', 'modules'])
+			.action(function (args, cb) {
+				if (rcon.connected) {
+					rcon.execute(`amxx ${args.command}`)
+						.then(cb)
+						.catch(cb);
+				}
+			});
+
+		vorpal
+			.delimiter(chalk.gray('$'))
+			.show();
+
+		vorpal.log(chalk.green('Connected successfully'));
+	};
+
+	const checkPassword = (buffer) => {
+		const msg = buffer.slice(5).toString('utf-8');
+		if (/^Bad rcon_password/.test(msg)) {
+			spinner.stop();
+			console.log(`${chalk.red('Error:')} ${chalk.yellow('Bad rcon password')}`);
+			rcon.disconnect();
+			process.exit(1);
+		} else if (/^logaddress_add:/.test(msg)) {
+			spinner.stop();
+			rcon.connection.removeListener('message', checkPassword);
+			rcon.connection.on('message', (buffer) => {
+				const msg = buffer.slice(4).toString('utf-8');
+				const match = msg.match(REGEX_LOG);
+				if (!match) {
+					vorpal.log(chalk.yellow(msg.slice(1)));
+				} else if (!REGEX_RCON.test(match[2])) {
+					vorpal.log(`${chalk.green(match[1])}: ${chalk.yellow(match[2])}`);
+				}
+			});
+			initConsole();
+		}
+	};
 
 	rcon
 		.connect(host, port)
@@ -37,57 +103,17 @@ const init = (host, port, password) => {
 				});
 		})
 		.then((address) => rcon.auth(address.address, address.port, password))
+		.then(() => {
+			rcon.connection.removeAllListeners('message');
+			rcon.connection.on('message', checkPassword)
+		})
 		.then(() => rcon.execute(`logaddress_del ${rcon.local.host} ${rcon.local.port}`))
 		.then(() => rcon.execute(`logaddress_add ${rcon.local.host} ${rcon.local.port}`))
-		.then(() => {
-			rcon.connection.on('message', (buffer) => {
-				const msg = buffer.slice(4).toString('utf-8');
-				const match = msg.match(REGEX_LOG);
-				if (!match) {
-					vorpal.log(chalk.yellow(msg.slice(1)));
-				} else if (!REGEX_RCON.test(match[2])) {
-					vorpal.log(`${chalk.green(match[1])}: ${chalk.yellow(match[2])}`);
-				}
-			});
-		})
 		.catch((error) => {
-			vorpal.log(chalk.red(`Error: ${error.message}`));
+			spinner.stop();
+			vorpal.log(`${chalk.red('Error:')} ${chalk.red(error.message)}`);
 			process.exit(1);
 		});
-
-	vorpal
-		.catch('<command>')
-		.action(function (args, cb) {
-			if (rcon.connected) {
-				rcon.execute(args.command)
-					.then(cb)
-					.catch(cb);
-			}
-		});
-
-	vorpal.command('meta [command]')
-		.autocomplete(['version', 'list'])
-		.action(function (args, cb) {
-			if (rcon.connected) {
-				rcon.execute(`meta ${args.command}`)
-					.then(cb)
-					.catch(cb);
-			}
-		});
-
-	vorpal.command('amxx [command]')
-		.autocomplete(['version', 'list', 'modules'])
-		.action(function (args, cb) {
-			if (rcon.connected) {
-				rcon.execute(`amxx ${args.command}`)
-					.then(cb)
-					.catch(cb);
-			}
-		});
-
-	vorpal
-		.delimiter(chalk.gray('$'))
-		.show();
 
 	process.on('exit', () => {
 		if (rcon.connected) {
